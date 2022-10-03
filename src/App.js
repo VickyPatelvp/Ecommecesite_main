4e1b4a204c31bf12ddd50400709ac2a67d338538
VickyPatelvp <pv788689@gmail.com>
2022-10-03 05:39:58 +0530

    add css files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component }  from 'react';
+import Home from './pages/Home';
+import Login from './MyComponents/Login';
+import Register from './MyComponents/Register';
+import Cart from './MyComponents/Cart';
+import Adminpanel from './MyComponents/Adminpanel';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='row'>
+      <Home/>
     </div>
+   
+<div className='row'>
+
+  </div>
+  <div className='row'>
+    <Cart></Cart>
+  </div>
+  <div>
+  <button class="scroltop fa fa-chevron-up" ></button>
+    </div>
+    <Adminpanel></Adminpanel>
+    </>
+  
   );
 }
 
