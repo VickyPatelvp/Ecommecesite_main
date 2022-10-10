580129b0ad43832740a3db10e81a53b030c99a0b
VickyPatelvp <pv788689@gmail.com>
2022-10-10 17:17:15 +0530

    save Changes

--- src/App.js
+++ src/App.js
@@ -13,46 +13,10 @@ function App() {
       {/* <div className='row'>
       <Home/>
     </div> */}
+     
+      </>
 
-      <div className="login-form">
-        <form>
-          <h1>Register</h1>
-          <div className="content">
-            <div className="row">
-              <div className="col-xs-12 col-sm-6">
-                <div className="input-field">
-                  <input type="text" placeholder="Firstname" autocomplete="nope" />
-                </div>
-              </div>
-              <div className="col-xs-12 col-sm-6">
-                <div className="input-field">
-                  <input type="text" placeholder="Lastname" autocomplete="nope" />
-                </div>  
-              </div>
-            </div>
-            <div className="input-field">
-              <input type="email" placeholder="Email" autocomplete="nope" />
-            </div>
-            <div className="input-field">
-              <input type="phone" placeholder="Phone" autocomplete="nope" />
-            </div>
-            <div className="input-field">
-              <input type="password" placeholder="Password" autocomplete="new-password" />
-            </div>
-            <a to="/login" className="link">Forgot Your Password?</a>
-          </div>
-          <div className="action">
-            <button className='active'>Register</button>
-            <button > Log in</button>
-          </div>
-        </form>
-      </div>
-
-      <Login/>
-      <Cart/>
-    </>
-
-  );
+      );
 }
 
-export default App;
+      export default App;
